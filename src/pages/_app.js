e45f41d6410a89b081b55be166dda6622e315186
Jose Tommaso <tommaso_j@hotmail.com>
2023-03-12 16:24:29 +0000

    contactForm provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Socials from '@/components/Socials';
+import { ContactFormProvider } from '@/context/contactForm';
 import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 
@@ -8,9 +9,11 @@ export default function App({ Component, pageProps, router }) {
 		<main className="relative bg-[url('/ffflux.svg')] bg-no-repeat bg-cover min-h-screen overflow-hidden">
 			<Header />
 			<Socials />
-			<AnimatePresence mode="wait" initial={true}>
-				<Component {...pageProps} key={router.asPath} />
-			</AnimatePresence>
+			<ContactFormProvider>
+				<AnimatePresence mode="wait" initial={true}>
+					<Component {...pageProps} key={router.asPath} />
+				</AnimatePresence>
+			</ContactFormProvider>
 		</main>
 	);
 }
