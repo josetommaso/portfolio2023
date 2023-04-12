a12a825356d1c1230d991cbc60b41da1609511ea
Jose <tommaso_j@hotmail.com>
2023-04-12 14:24:54 +0100

    GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Header from '@/components/Header';
 import Socials from '@/components/Socials';
 import { ContactFormProvider } from '@/context/contactForm';
@@ -6,14 +7,28 @@ import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps, router }) {
 	return (
-		<main className="relative bg-[url('/ffflux.svg')] bg-no-repeat bg-cover min-h-screen overflow-hidden">
-			<Header />
-			<Socials />
-			<ContactFormProvider>
-				<AnimatePresence mode="wait" initial={true}>
-					<Component {...pageProps} key={router.asPath} />
-				</AnimatePresence>
-			</ContactFormProvider>
-		</main>
+		<>
+			<Script
+				src={`https://www.googletagmanager.com/gtag/js?  id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+			</Script>
+			<main className="relative bg-[url('/ffflux.svg')] bg-no-repeat bg-cover min-h-screen overflow-hidden">
+				<Header />
+				<Socials />
+				<ContactFormProvider>
+					<AnimatePresence mode="wait" initial={true}>
+						<Component {...pageProps} key={router.asPath} />
+					</AnimatePresence>
+				</ContactFormProvider>
+			</main>
+		</>
 	);
 }
