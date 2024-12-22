75d1014b6bb9f5dbbaa4ffd9661a1473871027d6
Jose Tommaso <tommaso_j@hotmail.com>
2024-12-22 22:20:29 +0100

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,32 +1,32 @@
-import { AnimatePresence } from 'framer-motion';
-import Image from 'next/image';
-import { useState } from 'react';
-import Modal from './Modal';
+import { AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const Project = ({ project }) => {
-	const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-	const close = () => setModalOpen(false);
-	const open = () => setModalOpen(true);
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
 
-	return (
-		<>
-			<li className="overflow-hidden" onClick={modalOpen ? close : open}>
-				<Image
-					src={`/${project.file}`}
-					className="w-full h-full object-fill cursor-pointer hover:scale-105 transition duration-500"
-					alt={project.alt}
-					width="300"
-					height="200"
-				/>
-			</li>
-			<AnimatePresence initial={false}>
-				{modalOpen && (
-					<Modal modalOpen={modalOpen} handleClose={close} project={project} />
-				)}
-			</AnimatePresence>
-		</>
-	);
+  return (
+    <>
+      <li className="overflow-hidden" onClick={modalOpen ? close : open}>
+        <Image
+          src={`/${project.file}`}
+          className="w-full h-full object-cover cursor-pointer hover:scale-105 transition duration-500"
+          alt={project.alt}
+          width="300"
+          height="200"
+        />
+      </li>
+      <AnimatePresence initial={false}>
+        {modalOpen && (
+          <Modal modalOpen={modalOpen} handleClose={close} project={project} />
+        )}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default Project;
